e5597ca5f75ef0126d656f1701c229ddca3e608a
Thibault Dubois <dudub74@gmail.com>
2017-03-06 13:55:18 +0100

    implemented stat command

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -21,24 +21,6 @@ public class Server {
 
     private static Socket socket;
     private static String state;
-    private static String[] messages;
-    
-    static {
-        messages = new String[3];
-        messsages[0] = "From: John Doe <jdoe@machine.example>
-    To: Mary Smith <mary@example.net>
-    Subject: Saying Hello
-    Date: Fri, 21 Nov 1997 09:55:06 
-    -
-    0600
-    Message
-    -
-    ID: <1234@local.machine.example>
-    This is a message just to
-    say hello.
-    So, "Hello".
-    . "
-    }
     
     public static void main(String[] args) {
         Server.state = "closed";
@@ -64,57 +46,31 @@ public class Server {
             System.out.println("Err : fail to connect");
         } else {
             System.out.println("Client found");
-            try {
-                OutputStream os = Server.socket.getOutputStream();
-                BufferedOutputStream bos = new BufferedOutputStream(os);
-                byte[] data = "+OK POP3 server ready".getBytes();
-                bos.write(data);
-                bos.flush();
-            } catch (IOException ex) {
-                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-            }
+			write("+OK POP3 server ready");
         }
     }
     
     public static void process(){
-        try {
-            InputStream is = Server.socket.getInputStream();
-            byte[] data = new byte[2048];
-            int res = 0;
-            String result = "";
-            int offset = 0;
-            do{
-                res = is.read(data, offset, 0);
-                String s = new String(data);
-                result += s.trim();
-                data = new byte[2048];
-                offset+=res;
-            }while (res == 2048 && res != -1);
-            
-            String[] command = result.split(" ");
-            if (command.length == 1){
-                command[1]="-1";
-            }
-            switch(command[0]){
-                case("APOP") : 
-                    apop(command[1], command[2]);
-                    break;
-                case("STAT") : 
-                    stat();
-                    break;
-                case("RETR") : 
-                    retr(command[1]);
-                    break;
-                case("QUIT") : 
-                    quit();
-                    break;
-            }
-            
-        } catch (IOException ex) {
-            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        
+		String result = read();
+		
+		String[] command = result.split(" ");
+		if (command.length == 1){
+			command[1]="-1";
+		}
+		switch(command[0]){
+			case("APOP") : 
+				apop(command[1], command[2]);
+				break;
+			case("STAT") : 
+				stat();
+				break;
+			case("RETR") : 
+				retr(command[1]);
+				break;
+			case("QUIT") : 
+				quit();
+				break;
+		}
     }
 
     private static void apop(String name, String checksum) {
@@ -143,6 +99,10 @@ public class Server {
                 break;
                 
             case "transaction":
+				int number = readFile().split("./n").length;
+				int size = fileSize();
+				String answer = "+OK " + number + " " + size;
+				write(answer);
                 break;
                 
             case "update":
@@ -181,7 +141,64 @@ public class Server {
                 break;
         }
     }
+	
+	public static String readFile(){
+		BufferedReader br = new BufferedReader(new FileReader("mails.conf"));
+		String everything = "";
+		try {
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
 
-    
+			while (line != null) {
+				sb.append(line);
+				sb.append(System.lineSeparator());
+				line = br.readLine();
+			}
+			everything = sb.toString();
+		} finally {
+			br.close();
+			return everything;
+		}
+	}
+	
+	public static int fileSize(){
+		File file =new File("mails.conf");
+		if(file.exists()){
+			return file.length();
+		}
+		return -1;
+	}
 
+	public static String read(){
+		try{
+			InputStream is = Server.socket.getInputStream();
+			byte[] data = new byte[2048];
+			int res = 0;
+			String result = "";
+			int offset = 0;
+			do{
+				res = is.read(data, offset, 0);
+				String s = new String(data);
+				result += s.trim();
+				data = new byte[2048];
+				offset+=res;
+			}while (res == 2048 && res != -1);
+			return result;
+		} catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+			return "";
+		}
+	}
+	
+	public static void write(String message){
+		try {
+			OutputStream os = Server.socket.getOutputStream();
+			BufferedOutputStream bos = new BufferedOutputStream(os);
+			byte[] data = message.getBytes();
+			bos.write(data);
+			bos.flush();
+		} catch (IOException ex) {
+			Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 }
