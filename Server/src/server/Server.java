2d7ac33b84bc2f49817b1f9e6e195078ee3b7d39
Thibault Dubois <dudub74@gmail.com>
2017-03-07 10:00:40 +0100

    added retr and stat methodes clean connection

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -5,11 +5,17 @@
  */
 package server;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,13 +27,14 @@ public class Server {
 
     private static Socket socket;
     private static String state;
-    
+
     public static void main(String[] args) {
         Server.state = "closed";
         initSocket();
         connect();
         Server.state = "authorization";
-        //Server Ready here
+        Server.state = "transaction";
+        process();
 
     }
 
@@ -46,159 +53,177 @@ public class Server {
             System.out.println("Err : fail to connect");
         } else {
             System.out.println("Client found");
-			write("+OK POP3 server ready");
+            write("+OK POP3 server ready");
         }
     }
-    
-    public static void process(){
-		String result = read();
-		
-		String[] command = result.split(" ");
-		if (command.length == 1){
-			command[1]="-1";
-		}
-		switch(command[0]){
-			case("APOP") : 
-				apop(command[1], command[2]);
-				break;
-			case("STAT") : 
-				stat();
-				break;
-			case("RETR") : 
-				retr(command[1]);
-				break;
-			case("QUIT") : 
-				quit();
-				break;
-		}
+
+    public static void process() {
+        String result = read();
+
+        ArrayList<String> command = new ArrayList<String>(Arrays.asList(result.split(" ")));
+        if (command.size() == 1) {
+            command.add("-1");
+        }
+        switch (command.get(0)) {
+            case ("APOP"):
+                apop(command.get(1), command.get(2));
+                break;
+            case ("STAT"):
+                stat();
+                System.out.println("on est passés dans stat");
+                break;
+            case ("RETR"):
+                retr(command.get(1));
+                break;
+            case ("QUIT"):
+                quit();
+                break;
+        }
+        process();
     }
 
     private static void apop(String name, String checksum) {
-        switch (Server.state){
+        switch (Server.state) {
             case "closed":
                 break;
-                
+
             case "authorization":
                 break;
-                
+
             case "transaction":
                 break;
-                
+
             case "update":
                 break;
         }
-        
+
     }
-    
-     private static void stat() {
-         switch (Server.state){
+
+    private static void stat() {
+        switch (Server.state) {
             case "closed":
                 break;
-                
+
             case "authorization":
                 break;
-                
+
             case "transaction":
-				int number = readFile().split("./n").length;
-				int size = fileSize();
-				String answer = "+OK " + number + " " + size;
-				write(answer);
+                String mails = readFile();
+                String[] splitFile = mails.split("\r\n[.]\r\n");
+                int number = splitFile.length;
+                double size = mails.getBytes().length;
+                String answer = "+OK " + number + " " + size;
+                write(answer);
                 break;
-                
+
             case "update":
                 break;
         }
     }
 
-    private static void retr(String par) {
-        switch (Server.state){
+    private static void retr(String n) {
+        switch (Server.state) {
             case "closed":
                 break;
-                
+
             case "authorization":
                 break;
-                
+
             case "transaction":
+                String[] splitFile = readFile().split("\r\n[.]\r\n");
+                String mail = splitFile[Integer.parseInt(n)];
+                double size = mail.getBytes().length;
+                write("+OK " + size + " octets");
+                write(mail);
                 break;
-                
+
             case "update":
                 break;
         }
     }
 
     private static void quit() {
-        switch (Server.state){
+        switch (Server.state) {
             case "closed":
                 break;
-                
+
             case "authorization":
                 break;
-                
+
             case "transaction":
                 break;
-                
+
             case "update":
                 break;
         }
     }
-	
-	public static String readFile(){
-		BufferedReader br = new BufferedReader(new FileReader("mails.conf"));
-		String everything = "";
-		try {
-			StringBuilder sb = new StringBuilder();
-			String line = br.readLine();
 
-			while (line != null) {
-				sb.append(line);
-				sb.append(System.lineSeparator());
-				line = br.readLine();
-			}
-			everything = sb.toString();
-		} finally {
-			br.close();
-			return everything;
-		}
-	}
-	
-	public static int fileSize(){
-		File file =new File("mails.conf");
-		if(file.exists()){
-			return file.length();
-		}
-		return -1;
-	}
+    public static String readFile() {
+        BufferedReader br = null;
+        String everything = "";
+        try {
+            br = new BufferedReader(new FileReader("mails.conf"));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
 
-	public static String read(){
-		try{
-			InputStream is = Server.socket.getInputStream();
-			byte[] data = new byte[2048];
-			int res = 0;
-			String result = "";
-			int offset = 0;
-			do{
-				res = is.read(data, offset, 0);
-				String s = new String(data);
-				result += s.trim();
-				data = new byte[2048];
-				offset+=res;
-			}while (res == 2048 && res != -1);
-			return result;
-		} catch (IOException ex) {
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+            everything = sb.toString();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return everything;
+        }
+    }
+
+    /*
+    public static double fileSize() {
+        File file = new File("mails.conf");
+        if (file.exists()) {
+            return file.length();
+        }
+        return -1;
+    }
+    */
+
+    public static String read() {
+        try {
+            BufferedInputStream bis = new BufferedInputStream(Server.socket.getInputStream());
+            byte[] data = new byte[2048];
+            String result = "";
+            String currentChar = null;
+            String lastChar = null;
+            do {
+                lastChar = currentChar;
+                currentChar = Character.toString((char)bis.read());
+                result += currentChar;
+                data = new byte[2048];
+            }while (!(currentChar.contains("\n") && lastChar.contains("\r")));
+            result = result.substring(0,result.length()-2);
+            result = result.toUpperCase();
+            System.out.println("command: " + result);
+            return result;
+        } catch (IOException ex) {
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-			return "";
-		}
-	}
-	
-	public static void write(String message){
-		try {
-			OutputStream os = Server.socket.getOutputStream();
-			BufferedOutputStream bos = new BufferedOutputStream(os);
-			byte[] data = message.getBytes();
-			bos.write(data);
-			bos.flush();
-		} catch (IOException ex) {
-			Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-		}
-	}
+            return "";
+        }
+    }
+    
+    public static void write(String message) {
+        try {
+            OutputStream os = Server.socket.getOutputStream();
+            BufferedOutputStream bos = new BufferedOutputStream(os);
+            byte[] data = (message+"\r\n").getBytes();
+            bos.write(data);
+            bos.flush();
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
